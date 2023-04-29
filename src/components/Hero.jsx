3c6251ba5652b59c79f5a0197847cd95e1e7c6ae
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-04-29 22:10:00 +0300

    updated jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,12 +8,28 @@ export default function Hero() {
         }
     )
 
+    function handleChange() {
+
+    }
+
     return (
-        <section>
+        <section className="hero">
             <form action="" className="meme--from">
                 <div className="inputs-container">
-                    <input className="text--input" type="text" />
-                    <input className="text--input" type="text" />
+                    <input
+                        className="text--input"
+                        type="text"
+                        name="topText"
+                        onChange={handleChange}
+                        value={formData.topText}
+                    />
+                    <input
+                        className="text--input"
+                        type="text"
+                        name="bottomText"
+                        onChange={handleChange}
+                        value={formData.bottomText}
+                    />
                 </div>
                 <button className="get--meme-btn">Get new meme image</button>
             </form>
